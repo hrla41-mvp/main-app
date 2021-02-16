6c056d157f66b575963d9d40bedc1028b2f73a80
AntoineBalaine <abalaineaccts@gmail.com>
2021-02-15 21:17:59 -0800

    fixed server vs router bug

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 8080;
+const port = 3000;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.get('/', (req, res) => {
-//   res.status(200).sendFile('../public/index.html');
-// });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'), function (err) {
+    if (err) res.status(500).send(err);
+  })
+})
 
 io.on('connection', (socket) => {
   console.log('user connected');
