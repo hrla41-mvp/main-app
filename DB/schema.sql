98310d365719afec0128a5ead9b140a830e23b19
Dubeayi <dubeayi@gmail.com>
2021-02-17 09:29:08 -0800

    Added DB functionality

--- DB/schema.sql
+++ DB/schema.sql
@@ -1,38 +1,38 @@
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users(
-  user_id PRIMARY KEY,
+  user_id VARCHAR(100) PRIMARY KEY NOT NULL,
   first_name VARCHAR(40) UNIQUE NOT NULL,
   last_name VARCHAR(40) NOT NULL,
   cohort VARCHAR(100) NOT NULL,
   staff VARCHAR(10),
   friends TEXT[],
-  profile_pic TEXT[] NOT NULL UNIQUE,
+  profile_pic VARCHAR(100) NOT NULL UNIQUE,
   last_login TIMESTAMP,
   rooms TEXT[]
 --  FOREIGN KEY (cohort)
 --       REFERENCES Cohort(cohort_name),
 );
-DROP TYPE IF EXISTS Messages;
-CREATE TYPE Messages AS(
-  created_on TIMESTAMP,
-  user_id INT,
-  first_name VARCHAR(40),
-  profile_pic TEXT,
-  post VARCHAR(1000)
-  -- FOREIGN KEY (user_id)
-  --     REFERENCES Users (user_id),
-  -- FOREIGN KEY (first_name)
-  --     REFERENCES Users (first_name),
-  -- FOREIGN KEY (profile_pic)
-  --     REFERENCES Users (profile_pic),
-);
+-- DROP TYPE IF EXISTS Messages;
+-- CREATE TYPE Messages AS(
+--   created_on TIMESTAMP,
+--   user_id INT,
+--   first_name VARCHAR(40),
+--   profile_pic TEXT,
+--   post VARCHAR(1000)
+--   -- FOREIGN KEY (user_id)
+--   --     REFERENCES Users (user_id),
+--   -- FOREIGN KEY (first_name)
+--   --     REFERENCES Users (first_name),
+--   -- FOREIGN KEY (profile_pic)
+--   --     REFERENCES Users (profile_pic),
+-- );
 
 DROP TABLE IF EXISTS Rooms;
 CREATE TABLE Rooms(
-  room_id SERIAL,
+  room_id SERIAL NOT NULL,
   room_name VARCHAR(100),
   messages TEXT[],
-  users VARCHAR(100)
+  users TEXT[]
 );
 
 -- CREATE TABLE Cohort(
@@ -43,3 +43,35 @@ CREATE TABLE Rooms(
 --   messages Messages[],
 -- );
 
+
+--  {
+--      "user_id": "whateva",
+--      "first_name": "Anthony",
+--      "last_name": "Pecillo",
+--      "cohort": "hrla25",
+--      "staff": "true",
+--      "friends": ["none", "sike"],
+--      "profile_pic": "Nonejustyet",
+--      "rooms": ["FirstRoom", "SecondRoom"]
+--  }
+
+
+-- {
+--         "user_id": "whateva",
+--         "first_name": "Anthony",
+--         "last_name": "Pecillo",
+--         "cohort": "hrla25",
+--         "staff": "true",
+--         "friends": [
+--             "none",
+--             "sike",
+--             "rickRoss"
+--         ],
+--         "profile_pic": "http:yomama",
+--         "last_login": null,
+--         "rooms": [
+--             "One",
+--             "Two",
+--             "BuckleMyShoe"
+--         ]
+--     }
--- server/index.js
+++ server/index.js
@@ -4,25 +4,105 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000;
 const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const pool = require('../DB/index.js');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get(/\/(SignUp|MessageApp|Login)/, (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'), function (err) {
-    if (err) res.status(500).send(err);
-  })
-})
 
-app.get('/userInfo/:user_id', async (req, res)=> {
+app.post('/slackreactor/room', async (req, res) => {
   try {
-    const product = await pool.query(`SELECT * FROM Users WHERE user_id = ${req.params.user_id}`);
-    res.json(product.rows)
+    const { room_name, users } = req.body;
+    const user_id = req.body.message.user_id;
+    const first_name = req.body.message.first_name
+    const profile_pic = req.body.message.profile_pic
+    const post = req.body.message.message
+
+    const text = `{user_id: ${user_id}, first_name: ${first_name}, profile_pic: ${profile_pic}, message: ${post}}`
+    const stringified = JSON.stringify(text)
+    console.log('text ', text)
+
+    const query = `INSERT INTO Rooms (room_name, messages, users) VALUES('${room_name}', '{${stringified}}', '{${users}}');`
+
+    const newMessage = await pool.query(query);
+    res.json(query.rows)
   } catch (err) {
     console.error(err.message)
   }
 });
 
-app.get('/cohort', async (req, res) => {
+//FRIENDS -NEED TO ADD ANOTHER FRIEND
+//ROOMS - ADD ANOTHER ROOM
+//UPDATING A ROOM NAME -LOW
+
+app.post('/slackreactor/users', async (req, res) => {
+  console.log(req.body)
+  try {
+    const { room_name, users } = req.body;
+    const user_id = req.body.user_id;
+    const cohort = req.body.cohort;
+    const friends = req.body.friends;
+    const staff = req.body.staff;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const profile_pic = req.body.profile_pic;
+    const rooms = req.body.rooms;
+
+    const query = `INSERT INTO Users (user_id, cohort, friends, staff, first_name, last_name, profile_pic, rooms) VALUES('${user_id}', '${cohort}', '{${friends}}', '${staff}', '${first_name}', '${last_name}', '${profile_pic}', '{${rooms}}');`
+
+    const newMessage = await pool.query(query);
+    res.json(query.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
+app.put('/slackreactor/users/:id', async (req, res) => {
+  const itemToBeUpdated = req.params.id
+
+  if (req.body.rooms) {
+    try {
+      const body = JSON.stringify(req.body.rooms)
+      const formattedBody = body.slice(1, -1);
+      const room = '{' + formattedBody + '}'
+      const query = `UPDATE Users SET rooms = '${room}' WHERE user_id = '${itemToBeUpdated}'`
+      const dbQuery = await pool.query(query);
+      res.json(query.rows)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  if (req.body.friends) {
+    try {
+      const body = JSON.stringify(req.body.friends)
+      const formattedBody = body.slice(1, -1);
+      const friends = '{' + formattedBody + '}'
+      const query = `UPDATE Users SET friends = '${friends}' WHERE user_id = '${itemToBeUpdated}'`
+      const dbQuery = await pool.query(query);
+      res.json(query.rows)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  try {
+    const column = Object.keys(req.body)
+    console.log(req.body[column])
+    console.log(itemToBeUpdated)
+
+    const query = `UPDATE Users SET ${column} = '${req.body[column]}' WHERE user_id = '${itemToBeUpdated}'`
+    const dbQuery = await pool.query(query);
+    res.json(query.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
+app.get('/slackreactor/room', async (req, res) => {
   try {
     const product = await pool.query(`SELECT * FROM Rooms LIMIT 10`);
     res.json(product.rows)
@@ -31,40 +111,67 @@ app.get('/cohort', async (req, res) => {
   }
 });
 
+app.get('/slackreactor/users', async (req, res) => {
+  try {
+    const product = await pool.query(`SELECT * FROM Users LIMIT 10`);
+    res.json(product.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
+app.get(/\/(SignUp|MessageApp|Login)/, (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'), function (err) {
+    if (err) res.status(500).send(err);
+  })
+})
+
+app.get('/userInfo/:user_id', async (req, res) => {
+  try {
+    const product = await pool.query(`SELECT * FROM Users WHERE user_id = ${req.params.user_id}`);
+    res.json(product.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+});
+
+
 io.on('connection', (socket) => {
   console.log('user connected');
 
-  socket.on('room', (room)=> {
+  socket.on('room', (room) => {
     console.log('user joined room');
     socket.join(room);
   });
 
-  socket.on('add_user', user=> {
+  socket.on('add_user', user => {
     socket.emit('server_message', {
       name: user.name,
-      message:'welcome to the server!'
+      message: 'welcome to the server!'
     })
 
     socket.broadcast.emit('server_message', {
-      name:'server',
-      message:`${user.name} joined the chat`
+      name: 'server',
+      message: `${user.name} joined the chat`
     })
 
     socket.user = user;
   })
 
-  socket.on('message', ({ room, message })=> {
+  socket.on('message', ({ room, message }) => {
     console.log(room);
     console.log(message);
     io.to(room).emit('message', message);
   });
 
-  socket.on('disconnect', ()=> {
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   })
 });
 
 
+
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
