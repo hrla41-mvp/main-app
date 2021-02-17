0ce6f79e02c99a28513bc0043d0dd61097ffe42d
Dubeayi <dubeayi@gmail.com>
2021-02-16 19:54:59 -0800

    Added the chat functionality

--- src/components/RenderMessages.js
+++ src/components/RenderMessages.js
@@ -7,10 +7,10 @@ import { Button, Container, Row, Col } from 'react-bootstrap';
 // If passed in messages are from user render float right else render float left
 // This module should take in a (message , avatar, user, signedin user) signinuser to be able to tell whether or not we will render to the right or left
 const RenderMessages = ({ messages }) => {
-  const username = messages.username;
-  const avatar = messages.avatar;
+  const username = messages.username || '';
+  const avatar = messages.avatar || '';
   const message = messages.message;
-  const time = messages.time;
+  const time = messages.time || '';
   return (
     <>
       <div className="mb-3 messages myMessage">
